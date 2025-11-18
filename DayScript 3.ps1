5505880715f71c9801aede750a777c38bc1a41df
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2025-11-18 09:27:49 +0530

    range and list

---
+++ DayScript 3.ps1
@@ -0,0 +1,5 @@
+﻿$a = 6;
+$b = 8;
+
+# Write-Host $a.GetType() ($a+8);
+Write-Host "Hello i am Neeraj $($a+$b)";
---
+++ DayScript 4.ps1
@@ -0,0 +1,48 @@
+﻿# $a="Shivam Udit";
+# [int] $var1=5;
+# [int] $var2=8;
+
+# Write-Host "This is my String and value of: $($var1+$var2).GetType()";
+
+[String] $str1="Shivam";
+[String] $str2="Kumar";
+
+# Write-Host "The name is: $($str1+" "+$str2)"
+
+# Write-Host ($str1 -eq $str2);
+# Write-Host ($str1 -ne $str2);
+# Write-Host ($str1 -like "S*")
+# Write-Host ($str1 -notlike "Sh*")
+
+# Write-Host ($str1 -match "vaam")
+# Write-Host ($str1 -notmatch "vam")
+
+# $arr=@(3,5,7,8,9);
+
+# Write-Host $($arr -contains 9)
+# Write-Host $($arr -notcontains 93)
+
+# Write-Host $(3 -in $arr)
+# Write-Host $(3 -notin $arr)
+
+[int] $a=45;
+# $b="78";
+
+# Write-Host $($a -is [int])
+# Write-Host $($b -is [String])
+
+# $c=[int]$b
+
+# Write-Host $c
+# Write-Host "The sum is: $($($a+$b).GetType())";
+
+# Bitwise Operators
+$a=5
+$b=3
+
+Write-Host "Bitwise AND: " ($a -band $b)
+Write-Host "Bitwise OR: " ($a -bor $b)
+Write-Host "Bitwise XOR: " ($a -bxor $b)
+Write-Host "Bitwise NOT of a: " (-bnot $a)
+Write-Host "Left Shift: " ($a -shl 1)
+Write-Host "Right Shift: " ($a -shr 1)
