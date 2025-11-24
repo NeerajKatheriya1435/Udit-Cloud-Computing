161487ad5d6273e0954b565ef0ee55cf2973dc4b
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2025-11-24 10:47:24 +0530

    Notes And code powershell

---
+++ DayScript 2.ps1
@@ -0,0 +1,24 @@
+﻿$var1="Hello"
+$var2=45
+$udit=9.56
+
+[int]$sh3=56
+[int]$sh4=56
+[string]$sh5="Shivam Kumar"
+[bool]$bool1=$true
+$arr = @(1,2,3,4)
+$hash = @{Name="John"; Age=25}
+
+cls
+# Write-Host "The value is var1 is: $var1"
+# Write-Host "The value is var2 is: $var2"
+# Write-Host "The value is var3 is: $udit"
+# Write-Host "The value is var5 is: $sh3"
+#cWrite-Host "The value is var2 is: $sh4"
+#cWrite-Host "The value is var3 is: $sh5"
+# Write-Host "The value is var5 is: $bool1"
+# Write-Host "The value is var5 is: $arr"
+
+# $bool1.GetType().Name
+$sh5
+Write-Output "The name is : $sh5"
---
+++ DayScript 5.ps1
@@ -0,0 +1,63 @@
+﻿
+# write a ptogram to check wheather number positive negative or zero
+
+$a=0
+
+if($a -gt 0){
+Write-Host "Number is Positive"
+}
+
+elseif($a -lt 0){
+Write-Host "Number is Negative"
+}
+else{
+Write-Host "Number is 0"
+}
+
+$value=36
+
+switch($value){
+    1 {Write-Host "Sunday"}
+    2 {Write-Host "Monday"}
+    3 {Write-Host "Tuesday"}
+    4 {Write-Host "Wednesday"}
+    default {Write-Host "Please Choose number between 1 to 4"}
+}
+
+$a=6
+$b=4
+
+$c="+"
+
+switch($c){
+    "+" {Write-Host "The Sum is: $($a+$b) "}
+    "-" {Write-Host "The Sub is: $($a-$b)"}
+    "*" {Write-Host "The Mul is: $($a*$b)"}
+    "/" {Write-Host "The Div is: $($a/$b)"}
+    }
+
+# $i=1
+# sum of number 
+
+# find factirial and prime number problem
+
+$sum=0
+for ($i=1;$i -lt 6;$i++){
+    $sum+=$i;
+}
+
+Write-Host "The sum is: $sum"
+
+$a=1
+
+while($a -lt 6){
+Write-Host "Number is: $a"
+$a++
+}
+
+$b=13
+do{
+Write-Host $b
+$b++
+}while($b -lt 6)
+
