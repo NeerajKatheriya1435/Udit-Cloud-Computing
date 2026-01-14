2e9b9c52797b4696049600221a6d99a186a05f57
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2026-01-14 17:39:19 +0530

    ISE started

---
+++ Nawazish1.ps1
@@ -0,0 +1,14 @@
+﻿# Write-Host "Hello Script"
+# Copy-Item 
+# mkdir nawazish
+# Rohan Hello
+
+# Copy-Item "D:\\Cadd Mentor\\Student_Class_Code\\Udit Notes Cloud Computing\\Nawazish1.ps1" "D:\\Cadd Mentor\\Student_Class_Code\\Udit Notes Cloud Computing\\rohan.txt
+# Write-Host "Hello"
+# $PSVersionTable.PSVersion
+
+# Get-Process
+# Start-Service
+# Get-Process | Sort-Object CPU -Descending
+
+# xcopy "D:\Cadd Mentor\Student_Class_Code\Udit Notes Cloud Computing\rohan.txt" "D:\Cadd Mentor\Student_Class_Code\Udit Notes Cloud Computing\New Folder"
---
+++ Nawazish2.ps1
@@ -0,0 +1,29 @@
+﻿
+# [int] $var1 = 78
+# Write-Host "The value is : $var1"
+
+# [int] $var2 = 708
+# Write-Host "The value is : $var2"
+
+# [float] $var1 = 78.56
+# Write-Host "The value is : $var1"
+
+# [double] $var2 = 708.56778
+# Write-Host "The value is : $var2"
+
+# [string] $str1 = "John The Avenger"
+# Write-Host "My name is : $str1"
+
+# [bool] $bool1 = $true
+# Write-Host "My boolean value is : $bool1"
+
+$arr = @(4,6,8,8,4,3)
+Write-Host "My Array Value is : $arr"
+
+$dict1 = @({Apple="Red Fruit";Guawa="Green Fruit";Age=67})
+Write-Host $dict1.GetType()
+Write-Host "My Array value is : $dict1)"
+Write-Host "My Array type is : $($dict1.GetType())"
+# foreach ($item in $dict1){
+#    Write-Host "The value of item is: $item"
+# }
