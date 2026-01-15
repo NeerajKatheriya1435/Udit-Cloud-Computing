8009d3e0ebec6dfc53e00266e3512b86c84875c3
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2026-01-15 17:29:29 +0530

    Operators

---
+++ Nawazish3.ps1
@@ -0,0 +1,22 @@
+﻿# [float] $var1=56
+# Write-Host "The value is: $var1"
+
+# [bool] $var1=$false
+# Write-Host "The value is: $var1"
+
+# $array1=@(4,5,7,8,"Hindi",6,8,9,8.4567)
+$array1=@(4,5,7,8,6,8,9)
+Write-Host "The value is: $array1"
+Write-Host $array1.GetType()
+
+$array1=@{Name="Rohan";Age=67}
+Write-Host "The value is: $($array1.Keys)"
+Write-Host "The value is: $($array1.Values)"
+Write-Host "The value is: $($array1.Item("Name"))"
+Write-Host "The value is: $($array1.Item("Age"))"
+Write-Host $array1.GetType()
+
+[datetime]$dateTime = Get-Date
+
+Write-Host "The time is: $dateTime"
+Write-Host $dateTime.GetType()
---
+++ Nawazish4.ps1
@@ -0,0 +1,39 @@
+﻿
+# $a=9
+# $b=4
+
+# Write-Host "The sum is: $($a+$b)"
+
+# Write-Host "Addition: " ($a + $b)
+# Write-Host "Subtraction: " ($a - $b)
+# Write-Host "Multiplication: " ($a * $b)
+# Write-Host "Division: " ($a / $b)
+
+# Write-Host "Modulus: " ($a % $b)
+# $a++
+# $a--
+# $a++
+# Write-Host $a
+
+# $a=9
+# $b=9
+
+# Write-Host $($a -gt $b)  # 9 > 9
+# Write-Host $($a -ge $b)  # 9 >= 9
+
+# Write-Host $($a -lt $b)  #9 < 4
+# Write-Host $($a -le $b)
+
+# Write-Host $($a -eq $b)
+# Write-Host $($a -ne $b)
+
+$a=9
+$b=3
+
+# Write-Host $((5 -gt 2) -and (10 -lt 20) -and (5 -lt 4))
+
+Write-Host $((5 -gt 12) -or (110 -lt 20))
+
+Write-Host $(-not(5 -gt 12))
+
+Write-Host $(!(5 -gt 2))
