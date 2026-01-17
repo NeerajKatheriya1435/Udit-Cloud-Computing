3193a032ef1d0e92ae4cba23a21143c2cce4add9
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2026-01-17 17:13:42 +0530

    functions and break continue

---
+++ nawazish7.ps1
@@ -0,0 +1,73 @@
+
+# $age = 56
+
+# if ($age -lt 18) {
+#     Write-Host "You not can drive the car"
+# }
+# elseif ($age -lt 80) {
+#     Write-Host "You can drive the car"
+# }
+# else {
+#     Write-Host "You can not drive the car"
+# }
+
+# $day = 4
+
+# $day = 12
+
+# switch ($day) {
+#     1 { Write-Host "Today is Sunday" }
+#     12 { Write-Host "Today is Monday" }
+#     37 { Write-Host "Today is Tuesday" }
+#     4 { Write-Host "Today is Wednesday" }
+#     Default { Write-Host "Please Input 1 to 4" }
+# }
+
+# for ($i = 0; $i -lt 11; $i++ ) {
+#     Write-Host $($i * 5)
+# }
+# $i = 1
+
+# while ($i -lt 100) {
+#     Write-Host $i
+#     $i++
+# }
+
+# $i = 189898
+
+# do {
+#     Write-Host $i
+#     $i++
+# }while ($i -lt 100)
+    
+# $y = 1
+# do {
+#     Write-Output $y
+#     $y++
+# } until ($y -gt 3)
+
+
+# for ($i = 0; $i -lt 11; $i++ ) {
+#     if ($i -eq 5) {
+#         break
+#     }
+#     Write-Host $($i)
+# }
+# for ($i = 0; $i -lt 11; $i++ ) {
+#     if ($i -eq 5) {
+#         continue
+#     }
+#     Write-Host $($i)
+# }
+
+# function greet1() {
+#     Write-Host "Hello Good Morning"
+# }
+# function greet2() {
+#     Write-Host "Hello Good Evening"
+# }
+
+# greet1
+# greet2
+# greet2
+# greet1
